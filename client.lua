6e627df8c8325106e8c8636ae1e5ad6d3c4fcd2b
lucky04.lw <lucky04.lw@gmail.com>
2025-06-02 20:20:14 +0000

    Added x,y,z Items to CreateKeyLocker Menu

--- client.lua
+++ client.lua
@@ -16,9 +16,44 @@ end)
 RegisterCommand('CreateKeyLocker', function(source, args)
     ESX.TriggerServerCallback('Lux_KeyLockerv2:getGroup', function(group) 
         if group == 'admin' then 
-            print('test')
+            OpenCreateKeyLockerMenu()
         else
             ESX.ShowNotification(string.format(Translation[Config.Locale]['command_no_access']))
         end
     end)
 end, false)
+
+function OpenCreateKeyLockerMenu()
+    _menuPool:CloseAllMenus()
+
+    local PlayerPed = PlayerPedId()
+    local PlayerCoords = GetEntityCoords(PlayerPed)
+
+    local CreateKeyLockerMenu = NativeUI.CreateMenu(string.format(Translation[Config.Locale]['create_key_locker_menu_title']), '~b~')
+    _menuPool:Add(CreateKeyLockerMenu)
+
+    local CoordXItem = NativeUI.CreateItem('X:', '')
+    CoordXItem:RightLabel(ESX.Math.Round(PlayerCoords.x, 2))
+    CreateKeyLockerMenu:AddItem(CoordXItem)
+
+    local CoordYItem = NativeUI.CreateItem('Y:', '')
+    CoordYItem:RightLabel(ESX.Math.Round(PlayerCoords.y, 2))
+    CreateKeyLockerMenu:AddItem(CoordYItem)
+
+    local CoordZItem = NativeUI.CreateItem('Z:', '')
+    CoordZItem:RightLabel(ESX.Math.Round(PlayerCoords.z, 2))
+    CreateKeyLockerMenu:AddItem(CoordZItem)
+
+    local CloseItem = NativeUI.CreateItem(string.format(Translation[Config.Locale]['close_item_title']), string.format(Translation[Config.Locale]['close_item_description']))
+    CreateKeyLockerMenu:AddItem(CloseItem)
+    
+    CloseItem.Activated = function(sender, index)
+        _menuPool:CloseAllMenus()
+    end
+    
+    CreateKeyLockerMenu:Visible(true)
+    _menuPool:RefreshIndex()
+    _menuPool:MouseControlsEnabled(false)
+    _menuPool:MouseEdgeEnabled(false)
+    _menuPool:ControlDisablingEnabled(false)
+end
--- config.lua
+++ config.lua
@@ -7,6 +7,7 @@ Translation = {
         ['close_item_title'] = 'Schließen',
         ['close_item_description'] = '',
         ['command_no_access'] = '~r~Du hast hierauf keinen zugriff!',
+        ['create_key_locker_menu_title'] = 'Key Locker erstellen',
     },
     ['en'] = {
         ['close_item_title'] = 'close',
