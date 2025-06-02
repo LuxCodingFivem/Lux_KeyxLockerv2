6b4880220ea614c9cec9d746966b1d5215813e06
lucky04.lw <lucky04.lw@gmail.com>
2025-06-02 19:24:47 +0000

    Init Files

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.Locale = 'de' -- Translation en, de you can add more
+
+Translation = {
+    ['de'] = {
+        ['close_item_title'] = 'Schließen',
+        ['close_item_description'] = '',
+    },
+    ['en'] = {
+        ['close_item_title'] = 'close',
+        ['close_item_description'] = '',
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Luxcoding'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_scripts {
+	'@NativeUILua_Reloaded/src/NativeUIReloaded.lua',
+}
+
+escrow_ignore {
+    'config.lua',
+    'client.lua',
+    'server.lua'
+}
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'config.lua',
+    'server.lua'
+}
